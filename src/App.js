20b5fbe77069a25786b7679c63783dc56700c019
Orlando Montoya <omb0320@gmail.com>
2020-12-05 12:59:41 -0500

    Change the p tag message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div>
       <h1>Mi primera aplicación</h1>
-      <p>Esta es mi primera aplicación en React, está padrísimo</p>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
   );
 }
